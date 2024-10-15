00b93f89ed5df746f08fb67d566d04a8b2a3329c
Deydre <deydrealonso@gmail.com>
2024-10-15 14:01:00 +0200

    add/ base of js and changes in html

---
+++ script.js
@@ -0,0 +1,130 @@
+
+let results = [
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Brian May was the guitarist for which band?",
+        "correct_answer": "Queen",
+        "incorrect_answers": [
+            "Pink Floyd",
+            "Rolling Stones",
+            "The Doors"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which of the following is an album by punk rock band Anti-Flag?",
+        "correct_answer": "For Blood And Empire",
+        "incorrect_answers": [
+            "Infinity On High",
+            "21st Century Breakdown",
+            "No Pads, No Helmets...Just Balls"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which former boy-band star released hit solo single &quot;Angels&quot; in 1997?",
+        "correct_answer": "Robbie Williams",
+        "incorrect_answers": [
+            "Justin Timberlake",
+            "Harry Styles",
+            "Gary Barlow"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "&quot;Doctor Jones&quot;, &quot;Turn Back Time&quot; and &quot;Barbie Girl&quot; were UK number ones for which Eurodance group?",
+        "correct_answer": "Aqua",
+        "incorrect_answers": [
+            "Vengaboys",
+            "Eiffel 65",
+            "Sash!"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Who is the lead singer of Arctic Monkeys?",
+        "correct_answer": "Alex Turner",
+        "incorrect_answers": [
+            "Jamie Cook",
+            "Matt Helders",
+            "Nick O&#039;Malley"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which artist released the 2012 single &quot;Harlem Shake&quot;, which was used in numerous YouTube videos in 2013?",
+        "correct_answer": "Baauer",
+        "incorrect_answers": [
+            "RL Grime",
+            "NGHTMRE",
+            "Flosstradamus"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which classical composer wrote the &quot;Moonlight Sonata&quot;?",
+        "correct_answer": "Ludvig Van Beethoven",
+        "incorrect_answers": [
+            "Chief Keef",
+            "Wolfgang Amadeus Mozart",
+            "Johannes Brahms"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which Disney character sings the song &quot;A Dream is a Wish Your Heart Makes&quot;?",
+        "correct_answer": "Cinderella",
+        "incorrect_answers": [
+            "Belle",
+            "Snow White",
+            "Pocahontas"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Which Beatles album does NOT feature any of the band members on it&#039;s cover?",
+        "correct_answer": "The Beatles (White Album)",
+        "incorrect_answers": [
+            "Rubber Soul",
+            "Abbey Road",
+            "Magical Mystery Tour"
+        ]
+    },
+    {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Music",
+        "question": "Kanye West at 2009 VMA&#039;s interrupted which celebrity?",
+        "correct_answer": "Taylor Swift",
+        "incorrect_answers": [
+            "MF DOOM",
+            "Beyonce",
+            "Beck"]
+    }
+]
+
+// Recibir pregunta
+// Recibir respuestas
+
+
+// Pintar pregunta
+// Pintar cada una en un div
+
