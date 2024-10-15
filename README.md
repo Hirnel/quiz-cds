a274106e25c8fa1c2c580a15b537b902c8b5b3a1
Deydre <deydrealonso@gmail.com>
2024-10-15 18:24:11 +0200

    add/ chart

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# quiz-cds
+# 🎷🎵 Music Quizz 🎵🎷
+
+Welcome to our **Music Quizz**, created as part of the **Fullstack Bootcamp** we're taking at The Bridge. This project brings together some of the most important knowledge and skills we are learning.
+
+You can view the live website here: [Music Quizz](https://deydre.github.io/quizz/)
+
+
+## ✨ Features
+
+- **Responsive Design**: The website is optimized for different screen sizes, ensuring a smooth user experience across devices and using a Mobile First approach.
+- **Videogame UI**: The interface incorporates elements inspired by gaming design, enhancing engagement and making the quiz experience enjoyable and interactive.
+- **Dynamic API Integration**: Quiz questions are dynamically fetched from the Open Trivia Database API, ensuring variety with each session.
+- **Local Storage Management**: Game results are saved locally, allowing users to track their progress and review their statistics.
+- **Interactive Score Tracking**: Players' scores are displayed graphically with date-based progress tracking using Chartist.js.
+- **Replayability**: The game allows users to replay the quiz instantly with a simple click, refreshing questions for new gameplay experiences.
+
+
+
+## 📁 Project Structure
+
+```bash
+/
+├── /pages            # Subpages such as 'About Me', 'Portfolio', etc.
+├── /media            # Images, icons, and other assets
+├── index.html        # Main landing page
+├── script.js         # JS file
+└── styles.css        # CSS file
+```
+
+
+
+## 🚀 Technologies Used
+
+| Logo                                                                                           | Description                                                        |
+|------------------------------------------------------------------------------------------------|--------------------------------------------------------------------|
+| <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/2048px-HTML5_Badge.svg.png" alt="HTML5 Logo" height="30"> | **HTML5**: For the website's structure and layout.               |
+| <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" alt="CSS3 Logo" height="30"> | **CSS3**: To style the website and ensure responsive design.      |
+| <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JavaScript Logo" height="30"> | **JavaScript**: To create interactive and dynamic elements, especially for the mobile menu and the form inside the contact section. |
+| <img src="https://img.icons8.com/m_sharp/200/FFFFFF/github.png" alt="GitHub Logo" height="30"> | **GitHub Pages**: For hosting the live version of the website.    |
+
+
+
+
+## 📜 How to View the Project Locally
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/deydre/pagina-web-personal.git
+
+2. Open the index.html file in your browser.
+
+
+## 🌱 Challenges Faced
+- I found some complexity in creating the hamburger menu in CSS with Flexbox, but it was simple to animate in JS.
+- It was difficult to reorganize the project at the HTML and JS level when I moved the HTML files to the pages folder.
+- Validating the form with JS has been complicated when it comes to storing the inputs, labels, and clearly understanding what each thing is.
+
+## 🛠️ Future Improvements
+
+- UX / Make the menu and footer less intrusive when scrolling.
+- UX / Night Mode toggle for the entire website.
+- Icons / Take them from Font Awesome (not imgs)
+- Portfolio / Project filtering by tag.
+
+
+<h1 align= "center"> ✨🐸 Thanks for stopping by! 🐸✨ </h1>
+
+<div align="center">
+
+  <img src="/media/hiria_dev.jpg" alt="A cat with sunglasses programming" height="280">
+  
+  <a href="https://github.com/Deydre"> ⚡✨ My GitHub ✨⚡</a>
+  
+</div>
--- script.js
+++ script.js
@@ -4,52 +4,6 @@ if (!localStorage.getItem("Games")) {
     localStorage.setItem("Games", JSON.stringify([]));
 }
 
-
-// Conseguir con 10 preguntas nuestras, guardadas en un array de objetos, se pueda jugar a nuestro Quiz. [{..},{..},{..}...{..}]
-
-
-
-// Mostrar en la Home con una gráfica los resultados de las últimas partidas jugadas (leer puntuaciones de LocalStorage). Representar Fecha(eje X) vs Puntuación(eje Y)
-
-
-// Almacenar la puntuación de cada partida en un array de objetos [{..},{..},{..}...{..}] en Local Storage. Guardar puntuación y fecha en cada objeto del array
-
-// Obtener fecha en formato Día/Mes/Hora
-let currentDate = new Date();
-const day = currentDate.getUTCDate();
-const month = currentDate.getUTCMonth() + 1;
-const year = currentDate.getUTCFullYear(); 
-// Convertir a String
-let dateString = `${day}/${month}/${year}`;
-
-// OBJETO CON FECHA Y RESULTADOS
-let game = {
-    date: dateString,
-    data: [0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    score: Infinity // Calcular la puntuación después de crear el objeto
-};
-console.log("Objeto sin sumar score: ")
-console.log(game)
-
-//  SUMAR PUNTUACIÓN E INTRODUCIR EN EL OBJETO
-// Acceder sólo al array para sumar puntuación
-let dataResults = game.data;
-console.log("Array de aciertos dentro del objeto: ")
-console.log(dataResults)
-
-// Sumar puntuación
-game.score = dataResults.reduce(
-    (acc, current) => acc + current, 0,);
-console.log("Objeto con score sumado: ")
-console.log(game)
-
-// ALMACENAR OBJETO EN WEB STORAGE
-function updateGames(game) {
-    localStorage.setItem("Games", JSON.stringify(game));
-}
-
-updateGames(game);
-
 // ----------------------------------------------------------------------------------------------------- 
 
 const apiEasy = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";
@@ -57,7 +11,7 @@ const apiMedium = "https://opentdb.com/api.php?amount=10&category=12&difficulty=
 const apiHard = "https://opentdb.com/api.php?amount=10&category=12&difficulty=hard&type=multiple";
 
 
- // ------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------
 
 // Funciones para recibir la información de las API y transformarla en objetos
 async function getQuestionsEasy() {
@@ -192,10 +146,130 @@ printQuestions().then(datos => console.log(datos))
 
 // for (let i = 0; i < 10; i++) {  // Bucle for que ocurre 3 veces
 
-    //const div = document.getElementsByClassName('quiz-box')
-    // const img = document.createElement('img');
-    // const p = document.createElement('p');
+//const div = document.getElementsByClassName('quiz-box')
+// const img = document.createElement('img');
+// const p = document.createElement('p');
+
+
+// div.appendChild(pregunta);  // Introduce un h3 --> div
+
+// ------------------------------------------------------------------------------------------------
+
+
+
+
+// Conseguir con 10 preguntas nuestras, guardadas en un array de objetos, se pueda jugar a nuestro Quiz. [{..},{..},{..}...{..}]
+
+
+
+// Mostrar en la Home con una gráfica los resultados de las últimas partidas jugadas (leer puntuaciones de LocalStorage). Representar Fecha(eje X) vs Puntuación(eje Y)
+
+
+// Almacenar la puntuación de cada partida en un array de objetos [{..},{..},{..}...{..}] en Local Storage. Guardar puntuación y fecha en cada objeto del array
+
+// Obtener fecha en formato Día/Mes/Hora
+let currentDate = new Date();
+const day = currentDate.getUTCDate();
+const month = currentDate.getUTCMonth() + 1;
+const year = currentDate.getUTCFullYear();
+// Convertir a String
+let dateString = `${day}/${month}/${year}`;
+
+// OBJETO CON FECHA Y RESULTADOS
+let game = {
+    date: dateString,
+    data: [0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    score: Infinity // Calcular la puntuación después de crear el objeto
+};
+console.log("Objeto sin sumar score: ")
+console.log(game)
+
+//  SUMAR PUNTUACIÓN E INTRODUCIR EN EL OBJETO
+// Acceder sólo al array para sumar puntuación
+let dataResults = game.data;
+console.log("Array de aciertos dentro del objeto: ")
+console.log(dataResults)
+
+// Sumar puntuación
+game.score = dataResults.reduce(
+    (acc, current) => acc + current, 0,);
+console.log("Objeto con score sumado: ")
+console.log(game)
+
+// ALMACENAR OBJETO EN WEB STORAGE
+function updateGames(game) {
+    localStorage.setItem("Games", JSON.stringify(game));
+}
+
+updateGames(game);
+
+// Objeto simulado de varias partidas
+let games = [{
+    date: "05/04/24",
+    data: [0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    score: 7
+    },
+    {
+        date: "06/11/24",
+        data: [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+        score: 9
+    },
+    {
+        date: "15/12/24",
+        data: [1, 1, 0, 0, 0, 1, 0, 0, 1, 1],
+        score: 5
+    },
+]
+
+
+// PINTAR GRÁFICA CON FECHA + SCORE
+let arrayFechas = []; // Eje Y
+games.forEach(game => arrayFechas.push(game.date))
+
+let arrayScores = []; // Eje X
+games.forEach(game => arrayScores.push(game.score))
+
+console.log(arrayFechas, arrayScores)
+
+function printGraphic() {
+    new Chartist.Line('.chart', {
+        labels: arrayFechas,
+        series: [
+            arrayScores
+        ]
+    }, {
+        high: 10,
+        low: 0,
+        fullWidth: true,
+        // As this is axis specific we need to tell Chartist to use whole numbers only on the concerned axis
+        axisY: {
+            onlyInteger: true,
+            offset: 20
+        }
+    });
+
     
 
-    // div.appendChild(pregunta);  // Introduce un h3 --> div
-    
+    //   new Chartist.Line('.ct-chart', {
+    //     labels: [1, 2, 3, 4, 5, 6, 7, 8],
+    //     series: [
+    //       [1, 2, 3, 1, -2, 0, 1, 0],
+    //       [-2, -1, -2, -1, -3, -1, -2, -1],
+    //       [0, 0, 0, 1, 2, 3, 2, 1],
+    //       [3, 2, 1, 0.5, 1, 0, -1, -3]
+    //     ]
+    //   }, {
+    //     high: 3,
+    //     low: -3,
+    //     fullWidth: true,
+    //     // As this is axis specific we need to tell Chartist to use whole numbers only on the concerned axis
+    //     axisY: {
+    //       onlyInteger: true,
+    //       offset: 20
+    //     }
+    //   });
+}
+
+// Poner comentado cómo será cuando venga de LocalStorage
+
+printGraphic();
